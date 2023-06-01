a6e7d06076f855c114354e52d9096633d3f2964c
Yukiya <61388253+YukiyaVR@users.noreply.github.com>
2023-06-01 18:08:13 +0200

    Add files via upload

---
+++ File readerModule.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using VRCOSC.Game.Modules;
+using VRCOSC.Game.Modules.ChatBox;
+using VRCOSC.Game.OSC.VRChat;
+
+namespace VRCOSC.Modules.FileReader
+{
+    public class FileReaderModule : ChatBoxModule
+    {
+        public override string Title => "File Reader";
+        public override string Description => "Reads the contents of a text file";
+        public override string Author => "YukiyaVR";
+        public override ModuleType Type => ModuleType.General;
+
+        protected override TimeSpan DeltaUpdate => TimeSpan.FromSeconds(1.5);
+
+        private string? filePath;
+        private string? fileContent;
+
+        protected override void CreateAttributes()
+        {
+            CreateSetting(FileReaderSetting.FilePath, "File Path", "The path to the text file", string.Empty);
+            CreateVariable(FileReaderVariable.FileContent, "File Content", "content");
+            CreateState(FileReaderState.Default, "Default", $"{GetVariableFormat(FileReaderVariable.FileContent)}");
+            CreateEvent(FileVariableChanged.TextUpdate, "Content Changed", $"{GetVariableFormat(FileReaderVariable.FileContent)}", 4        );
+
+        }
+
+        protected override void OnModuleStart() 
+        {
+            ChangeStateTo(FileReaderState.Default);
+            filePath = GetSetting<string>(FileReaderSetting.FilePath);
+            if (IsValidFilePath())
+            {
+                Log("File path is valid. Reading file content...");
+            }
+            else
+            {
+                Log("Invalid file path. Please provide a valid file path in the module settings.");
+            }
+        }
+
+        protected override void OnModuleUpdate()
+        {
+            if (IsValidFilePath())
+            {
+                ReadFileContent();
+            }               
+        }
+
+        private bool IsValidFilePath()
+        {
+            return !string.IsNullOrEmpty(filePath) && System.IO.File.Exists(filePath);
+        }
+
+        private void ReadFileContent()
+        {
+            try
+            {
+                if (!string.IsNullOrEmpty(filePath) && System.IO.File.Exists(filePath))
+                {
+                    string newFileContent = System.IO.File.ReadAllText(filePath);
+                    if (newFileContent != fileContent)
+                    {
+                        TriggerEvent(FileVariableChanged.TextUpdate);
+                        fileContent = newFileContent;
+                        SetVariableValue(FileReaderVariable.FileContent, fileContent);
+                    }
+                }
+                else
+                {
+                    fileContent = string.Empty;
+                    SetVariableValue(FileReaderVariable.FileContent, fileContent);
+                }
+            }
+            catch (Exception ex)
+            {
+                Log($"Error reading file: {ex.Message}");
+                fileContent = string.Empty;
+                SetVariableValue(FileReaderVariable.FileContent, fileContent);
+            }
+        }
+
+        private enum FileReaderSetting
+        {
+            FilePath
+        }
+
+        private enum FileReaderState
+        {
+            Default
+        }
+
+        private enum FileReaderVariable
+        {
+            FileContent
+        }
+        private enum FileVariableChanged
+        {
+            TextUpdate
+        }
+    }
+}
