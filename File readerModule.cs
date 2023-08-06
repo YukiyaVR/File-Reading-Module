fa64b43ff4b4cc7f1455c57152d15f2ea3fb2768
Yukiya <61388253+YukiyaVR@users.noreply.github.com>
2023-08-06 15:44:38 +0200

    Update File readerModule.cs

--- File readerModule.cs
+++ File readerModule.cs
@@ -1,103 +1,97 @@
-﻿using System;
+using System;
 using VRCOSC.Game.Modules;
-using VRCOSC.Game.Modules.ChatBox;
-using VRCOSC.Game.OSC.VRChat;
+using VRCOSC.Game.Modules.Avatar;
 
-namespace VRCOSC.Modules.FileReader
+namespace VRCOSC.Modules.FileReader;
+
+[ModuleTitle("File reader")]
+[ModuleDescription("Reads the contents of a text file")]
+[ModuleAuthor("YukiyaVR", "https://github.com/YukiyaVR")]
+[ModuleGroup(ModuleType.General)]
+
+public sealed class FileReaderModule : ChatBoxModule
 {
-    public class FileReaderModule : ChatBoxModule
+    private string? filePath;
+    private string? fileContent;
+    protected override void CreateAttributes()
     {
-        public override string Title => "File Reader";
-        public override string Description => "Reads the contents of a text file";
-        public override string Author => "YukiyaVR";
-        public override ModuleType Type => ModuleType.General;
+        CreateSetting(FileReaderSetting.FilePath, "File Path", "The path to the text file", string.Empty);
+        CreateVariable(FileReaderVariable.FileContent, "File Content", "content");
+        CreateState(FileReaderState.Default, "Default", $"{GetVariableFormat(FileReaderVariable.FileContent)}");
+        CreateEvent(FileVariableChanged.TextUpdate, "Content Changed", $"{GetVariableFormat(FileReaderVariable.FileContent)}", 4);
 
-        protected override TimeSpan DeltaUpdate => TimeSpan.FromSeconds(1.5);
+    }
 
-        private string? filePath;
-        private string? fileContent;
-
-        protected override void CreateAttributes()
+    protected override void OnModuleStart()
+    {
+        ChangeStateTo(FileReaderState.Default);
+        filePath = GetSetting<string>(FileReaderSetting.FilePath);
+        if (IsValidFilePath())
         {
-            CreateSetting(FileReaderSetting.FilePath, "File Path", "The path to the text file", string.Empty);
-            CreateVariable(FileReaderVariable.FileContent, "File Content", "content");
-            CreateState(FileReaderState.Default, "Default", $"{GetVariableFormat(FileReaderVariable.FileContent)}");
-            CreateEvent(FileVariableChanged.TextUpdate, "Content Changed", $"{GetVariableFormat(FileReaderVariable.FileContent)}", 4        );
-
+            Log("File path is valid. Reading file content...");
         }
-
-        protected override void OnModuleStart() 
+        else
         {
-            ChangeStateTo(FileReaderState.Default);
-            filePath = GetSetting<string>(FileReaderSetting.FilePath);
-            if (IsValidFilePath())
-            {
-                Log("File path is valid. Reading file content...");
-            }
-            else
-            {
-                Log("Invalid file path. Please provide a valid file path in the module settings.");
-            }
+            Log("Invalid file path. Please provide a valid file path in the module settings.");
         }
+    }
 
-        protected override void OnModuleUpdate()
+    [ModuleUpdate(ModuleUpdateMode.Custom)]
+    private void updateVariables()
+    {
+        if (IsValidFilePath())
         {
-            if (IsValidFilePath())
-            {
-                ReadFileContent();
-            }               
+            ReadFileContent();
         }
+    }
 
-        private bool IsValidFilePath()
+    private bool IsValidFilePath()
+    {
+        return !string.IsNullOrEmpty(filePath) && System.IO.File.Exists(filePath);
+    }
+    private void ReadFileContent()
+    {
+        try
         {
-            return !string.IsNullOrEmpty(filePath) && System.IO.File.Exists(filePath);
-        }
-
-        private void ReadFileContent()
-        {
-            try
+            if (!string.IsNullOrEmpty(filePath) && System.IO.File.Exists(filePath))
             {
-                if (!string.IsNullOrEmpty(filePath) && System.IO.File.Exists(filePath))
+                string newFileContent = System.IO.File.ReadAllText(filePath);
+                if (newFileContent != fileContent)
                 {
-                    string newFileContent = System.IO.File.ReadAllText(filePath);
-                    if (newFileContent != fileContent)
-                    {
-                        TriggerEvent(FileVariableChanged.TextUpdate);
-                        fileContent = newFileContent;
-                        SetVariableValue(FileReaderVariable.FileContent, fileContent);
-                    }
-                }
-                else
-                {
-                    fileContent = string.Empty;
+                    TriggerEvent(FileVariableChanged.TextUpdate);
+                    fileContent = newFileContent;
                     SetVariableValue(FileReaderVariable.FileContent, fileContent);
                 }
             }
-            catch (Exception ex)
+            else
             {
-                Log($"Error reading file: {ex.Message}");
                 fileContent = string.Empty;
                 SetVariableValue(FileReaderVariable.FileContent, fileContent);
             }
         }
-
-        private enum FileReaderSetting
+        catch (Exception ex)
         {
-            FilePath
-        }
-
-        private enum FileReaderState
-        {
-            Default
-        }
-
-        private enum FileReaderVariable
-        {
-            FileContent
-        }
-        private enum FileVariableChanged
-        {
-            TextUpdate
+            Log($"Error reading file: {ex.Message}");
+            fileContent = string.Empty;
+            SetVariableValue(FileReaderVariable.FileContent, fileContent);
         }
     }
-}
+
+    private enum FileReaderSetting
+    {
+        FilePath
+    }
+
+    private enum FileReaderState
+    {
+        Default
+    }
+
+    private enum FileReaderVariable
+    {
+        FileContent
+    }
+    private enum FileVariableChanged
+    {
+        TextUpdate
+    }
